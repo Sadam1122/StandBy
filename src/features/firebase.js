84c4ad18d5daf6e3bb70414b449ab4a3ee3700b4
Sadam1122 <sadamalrasyid1@gmail.com>
2024-07-30 12:43:05 +0700

    dam

---
+++ src/features/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database"; // Import getDatabase from firebase/database
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB-C5LiItlozMBNSKhGKA-XXMtPwMwEj9A",
+    authDomain: "standby-c50ba.firebaseapp.com",
+    databaseURL: "https://standby-c50ba-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "standby-c50ba",
+    storageBucket: "standby-c50ba.appspot.com",
+    messagingSenderId: "627233552935",
+    appId: "1:627233552935:web:a89e271cbd1e22ec3bac02"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app); // Correctly use getDatabase
